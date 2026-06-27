a294a3ce6009dcfbd93f5c33c66fc8c98864a59a
www <1mullermoni@gmail.com>
2026-06-27 08:28:53 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const fs = require("fs");
 const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
 
-service:"gmail",
+host:"smtp.gmail.com",
+
+port:587,
+
+secure:false,
 
 auth:{
 
